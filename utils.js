2422ef0fde03b6d2ea8d2036f4492883552fd77e
Egidijus <noneede@gmail.com>
2018-09-01 04:31:42 +0000

    quiueUrl now has full new request functionality eg: uniqueId, hash support

--- utils.js
+++ utils.js
@@ -71,7 +71,7 @@ function utils(Apify, requestQueue){
       }
       
       if(url && url.length){
-        await reqQueue.addRequest(new Apify.Request({ url, userData }));
+        await reqQueue.addRequest(new Apify.Request({ ...urlObj, url, userData }));
         this.debug && console.log(`[MATCHER] Queued ${requestPendingCount(reqQueue)}`, trunc(url, 150, true), { userDataSize: Object.keys(userData).length });
         userData.initial && this.initialRequestsAmount++;
         
